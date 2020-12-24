74013198ecb8abbb94b4788bdb0db371883caac5
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-12-24 16:12:58 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+import setuptools
 
+setuptools.setup(
+    name="aiomojang",
+    version="0.5",
+    author="capslock321",
+    description="A Asynchronous python wrapper of Mojangs API",
+    url="https://github.com/capslock321/aiomojang/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
