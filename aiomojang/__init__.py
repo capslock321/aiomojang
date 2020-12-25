46a87328cf49f9eb293d4ba617c853f51d9260a8
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-12-25 18:18:11 -0500

    add one last ting i forgot

--- aiomojang/__init__.py
+++ aiomojang/__init__.py
@@ -1,5 +1,5 @@
 from aiomojang.exceptions import BadRequestException, IncorrectPayload, ApiException, MismatchedInputException, RequiredPayload, UserNotFound
 from aiomojang.misc import Statistics, BlockedServers
-from aiomojang.renders import Skin, Cape
+from aiomojang.renders import Skin, Cape, Render
 from aiomojang.status import Status
 from aiomojang.user import User
