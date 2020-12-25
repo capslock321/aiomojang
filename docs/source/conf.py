833b5955973c035365fc5dd37dd6c89f5fd4c80a
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-12-25 11:39:14 -0500

    index for doc building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,8 @@ release = '0.6'
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
