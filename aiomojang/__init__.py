f433781388a34b82d017f8e1f1d815acc480565c
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-12-27 18:00:16 -0500

    update init

--- aiomojang/__init__.py
+++ aiomojang/__init__.py
@@ -2,4 +2,4 @@ from aiomojang.exceptions import BadRequestException, IncorrectPayload, ApiExcep
 from aiomojang.misc import Statistics, BlockedServers
 from aiomojang.renders import Skin, Cape, Render
 from aiomojang.status import Status
-from aiomojang.user import User
+from aiomojang.user import Users, Player
