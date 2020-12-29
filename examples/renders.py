fca9faa846ca9c7bd1616b6a33171792caf210dd
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-12-28 21:57:38 -0500

    skins and capes
    example 2

---
+++ examples/renders.py
@@ -0,0 +1,12 @@
+import aiomojang
+import asyncio
+
+async def get_user_skins():
+    # If you want a user's cape, you can run aiomojang.Cape() instead.
+    skin = aiomojang.Skin("Hypermnesia")
+    # This may be confusing, but by passing in a user's name, you are getting their skin.
+    # You can get a skins url using skin.url()
+    # If you only need the last part of the link, you can do skin.id()
+    return await skin.url()
+
+asyncio.run(get_user_skins())  # Running the function
