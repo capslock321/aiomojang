09010b55dfcd8a7a402acf6fd8bdf65349952515
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-12-30 17:22:20 -0500

    whoops

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="aiomojang",
-    version="0.2",
+    version="0.3",
     author="capslock321",
     description="A Asynchronous python wrapper of Mojangs API",
     url="https://github.com/capslock321/aiomojang/",
