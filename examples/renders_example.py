e8a83218db5f9fef15f8bf96fcd89dd0af3e2e96
capslock321 <69331371+capslock321@users.noreply.github.com>
2021-03-08 18:39:45 -0500

    both props
    both are properties

--- examples/renders_example.py
+++ examples/renders_example.py
@@ -5,8 +5,8 @@ async def get_user_skins():
     # If you want a user's cape, you can run aiomojang.Cape() instead.
     skin = aiomojang.Skin("Hypermnesia")
     # This may be confusing, but by passing in a user's name, you are getting their skin.
-    # You can get a skins url using skin.url()
-    # If you only need the last part of the link, you can do skin.id()
+    # You can get a skins url using skin.url
+    # If you only need the last part of the link, you can do skin.id
     return await skin.url
 
 asyncio.run(get_user_skins())  # Running the function
