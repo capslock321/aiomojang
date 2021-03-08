83994b291f08970ec05bdfcfd8cb2d192f6070b2
capslock321 <69331371+capslock321@users.noreply.github.com>
2021-03-08 17:37:20 -0500

    changed url into a property
    i have not pushed that to pypi yet tho

--- examples/renders_example.py
+++ examples/renders_example.py
@@ -7,6 +7,6 @@ async def get_user_skins():
     # This may be confusing, but by passing in a user's name, you are getting their skin.
     # You can get a skins url using skin.url()
     # If you only need the last part of the link, you can do skin.id()
-    return await skin.url()
+    return await skin.url
 
 asyncio.run(get_user_skins())  # Running the function
